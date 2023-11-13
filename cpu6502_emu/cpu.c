46e2cf3b7dff049e2b78b21094174d3357ae91ef
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2023-11-13 21:33:10 +0100

    Add more instructions

--- cpu6502_emu/cpu.c
+++ cpu6502_emu/cpu.c
@@ -1,5 +1,73 @@
 #include "cpu.h"
 #include <stdio.h>
+#include <stdbool.h>
+
+byte readByteFromPC(word* pc, const Mem* mem, u32* cycles) {
+   byte data = mem->data[*pc];
+   (*pc)++;
+   (*cycles)--;
+
+   return data;
+}
+
+byte readByteFromAddr(word addr, const Mem* mem, u32* cycles) {
+   byte data = mem->data[addr];
+   (*cycles)--;
+
+   return data;
+}
+
+word readWordFromPC(word* pc, const Mem* mem, u32* cycles) {
+   byte loB = readByteFromPC(pc, mem, cycles);
+   byte hiB = readByteFromPC(pc, mem, cycles);
+
+#ifdef DEBUG
+   printf("\tRead word. loB=[0x%X], hiB=[0x%X]\n", loB, hiB);
+#endif // DEBUG
+
+   return (word)(loB | (hiB << 8));
+}
+
+word readWordFromAddr(word addr, const Mem* mem, u32* cycles) {
+   byte loB = readByteFromAddr(addr, mem, cycles);
+   byte hiB = readByteFromAddr(addr + 1, mem, cycles);
+
+#ifdef DEBUG
+   printf("\tRead word from [0x%X+0x%X]. loB=[0x%X], hiB=[0x%X]\n", addr, addr + 1, loB, hiB);
+#endif // DEBUG
+
+   return (word)(loB | (hiB << 8));
+}
+
+void pushWordToStack(Mem* mem, byte* sp, word val, u32* cycles) {
+   (*sp)--;
+   mem->data[*sp] = val & 0xFF;
+   (*sp)--;
+   mem->data[*sp] = val >> 8;
+
+   (*cycles) -= 2;
+
+#ifdef DEBUG
+   printf("\tPushed [0x%X] to the stack. Current sp: [0x%X]\n", val, *sp);
+#endif // DEBUG
+
+}
+
+word popWordFromStack(const Mem* mem, byte* sp, u32* cycles) {
+   byte hB = mem->data[*sp];
+   (*sp)++;
+   byte lB = mem->data[*sp];
+   (*sp)++;
+   (*cycles) -= 4;
+
+   word data = (word)((hB << 8) | lB);
+   return data;
+}
+
+void setNAndZFlags(CPU* cpu) {
+   cpu->z = (cpu->a == 0);
+   cpu->n = (cpu->a & 0b10000000) > 0;
+}
 
 void reset(CPU* cpu, Mem* mem) {
    cpu->pc = 0xFFFC;
@@ -27,76 +95,77 @@ void initMem(Mem* mem) {
    }
 }
 
-byte readByte(word* pc, const Mem* mem, u32* cycles) {
-   byte data = mem->data[*pc];
-   (*pc)++;
-
-   (*cycles)--;
-   return data;
-}
-
-word readWord(word* pc, const Mem* mem, u32* cycles) {
-   word data = mem->data[*pc];
-   (*pc)++;
-
-   data |= (mem->data[*pc] << 8);
-   (*pc)++;
-   (*cycles) -= 2;
-
-#ifdef DEBUG
-   printf("\tRead word: 0x%X\n", data);
-#endif // DEBUG
-
-   return data;
-}
-
-void pushWordToStack(Mem* mem, byte* sp, word val, u32* cycles) {
-   mem->data[*sp] = val & 0xFF;
-   (*sp)--;
-   mem->data[*sp] = val >> 8;
-   (*sp)--;
-
-   (*cycles) -= 2;
-
-#ifdef DEBUG
-   printf("\tPushed [0x%X] to the stack. Current sp: [0x%X]\n", val, *sp);
-#endif // DEBUG
-
-}
-
-word popWordFromStack(const Mem* mem, byte* sp, u32* cycles) {
-   word data = mem->data[*sp]; //lB
-   (*sp)++;
-   data |= (mem->data[*sp] << 8);
-   (*sp)++;
-   (*cycles) -= 2;
-
-   return data;
-}
-
-void setLDAFlags(CPU* cpu) {
-   cpu->z = (cpu->a == 0);
-   cpu->n = (cpu->a & 0b10000000) > 0;
-}
-
 void exec(CPU* cpu, Mem* mem, u32 cycles) {
    while (cycles > 0) {
-      byte nextIns = readByte(cpu, mem, &cycles);
+      byte nextIns = readByteFromPC(cpu, mem, &cycles);
 #ifdef DEBUG
       printf("Instruction: 0x%X\n", nextIns);
 #endif
       switch (nextIns) {
       case INS_JSR: {
-         word addr = readWord(&cpu->pc, mem, &cycles);
-         pushWordToStack(mem, &cpu->sp, addr, &cycles);
+         word addr = readWordFromPC(&cpu->pc, mem, &cycles);
+         pushWordToStack(mem, &cpu->sp, cpu->pc - 1, &cycles);
 
          cpu->pc = addr;
          cycles--;
       } break;
+      case INS_RTS: {
+         word addr = popWordFromStack(mem, &cpu->sp, &cycles);
+         cpu->pc = addr;
+         cycles--;
+      } break;
       case INS_LDA_IM: {
-         byte val = readByte(&cpu->pc, mem, &cycles);
+         byte val = readByteFromPC(&cpu->pc, mem, &cycles);
          cpu->a = val;
-         setLDAFlags(cpu);
+         setNAndZFlags(cpu);
+      } break;
+      case INS_LDA_ZP: {
+         byte zpAddr = readByteFromPC(&cpu->pc, mem, &cycles);
+         cpu->a = readByteFromAddr(zpAddr, mem, &cycles);
+         setNAndZFlags(cpu);
+      } break;
+      case INS_LDA_ZPX: {
+         byte zpAddr = readByteFromPC(&cpu->pc, mem, &cycles);
+         zpAddr += cpu->x;
+
+         cpu->a = readByteFromAddr(zpAddr, mem, &cycles);
+         setNAndZFlags(cpu);
+      } break;
+      case INS_LDA_ABS: {
+         word absAddr = readWordFromPC(&cpu->pc, mem, &cycles);
+         cycles--;
+
+         cpu->a = readByteFromAddr(absAddr, mem, &cycles);
+         setNAndZFlags(cpu);
+      } break;
+      case INS_LDA_ABSX: {
+         word absAddr = readWordFromPC(&cpu->pc, mem, &cycles);
+         word absAddrX = absAddr + cpu->x;
+         const bool crossed = (absAddr ^ absAddrX) >> 8;
+         if (crossed) {
+            cycles--;
+         }
+         cpu->a = readByteFromAddr(absAddrX, mem, &cycles);
+         setNAndZFlags(cpu);
+      } break;
+      case INS_LDA_ABSY: {
+         word absAddr = readWordFromPC(&cpu->pc, mem, &cycles);
+         word absAddrY = absAddr + cpu->y;
+         const bool crossed = (absAddr ^ absAddrY) >> 8;
+         if (crossed) {
+            cycles--;
+         }
+         cpu->a = readByteFromAddr(absAddrY, mem, &cycles);
+         setNAndZFlags(cpu);
+      } break;
+      case INS_LDA_INDX: {
+         byte addrZp = readByteFromPC(&cpu->pc, mem, &cycles);
+         addrZp += cpu->x;
+         cycles--;
+         word addr = readWordFromAddr(addrZp, mem, &cycles);
+         
+         cpu->a = readByteFromAddr(addr, mem, &cycles);
+         setNAndZFlags(cpu);
       } break;
       default: {
          printf("Unsupported instruction: %d", nextIns);
--- cpu6502_emu/cpu.h
+++ cpu6502_emu/cpu.h
@@ -3,45 +3,57 @@
 #define CPU_H
 #define DEBUG
 
+#ifdef _cplusplus
+extern "C" {
+#endif // _cplusplus
+
 #include <stdlib.h>
 
-#define MEM_MAX 65536   //(64 * 1024)
-#define INS_JSR 0x20    //Jump to Subroutine. Takes 6 cycles.
-#define INS_RTS 0x60    //Return to the calling routine. Takes 6 cycles.
-#define INS_LDA_IM 0xA9 //Immediate. Takes 2 cycles.
-#define INS_LDA_ZP
+#define MEM_MAX 65536      //(64 * 1024)
+#define INS_JSR 0x20       //Jump to Subroutine. Takes 6 cycles.
+#define INS_RTS 0x60       //Returns to the calling routine. Takes 6 cycles.
+#define INS_LDA_IM 0xA9    //Load accumulator immediate. Takes 2 cycles.
+#define INS_LDA_ZP 0xA5    //Zero page. Takes 3 cycles.
+#define INS_LDA_ZPX 0xB5   //Zero page X. Takes 4 cycles.
+#define INS_LDA_ABS 0xAD   //Absolute. Takes 4 cycles.
+#define INS_LDA_ABSX 0xBD  //Absolute X. Takes 4(+1) cycles.
+#define INS_LDA_ABSY 0xB9  //Absolute Y. Takes 4(+1) cycles
+#define INS_LDA_INDX 0xA1  //Indirect X. Takes 6 cycles.
 
-typedef unsigned char byte;
-typedef unsigned short word;
-typedef unsigned int u32;
+   typedef unsigned char byte;
+   typedef unsigned short word;
+   typedef unsigned int u32;
 
-struct Mem {
-   byte data[MEM_MAX];
-};
+   struct Mem {
+      byte data[MEM_MAX];
+   };
 
-struct CPU {
-   word pc;
-   byte sp;
+   struct CPU {
+      word pc;
+      byte sp;
 
-   byte a;
-   byte x;
-   byte y;
-   byte ps;
+      byte a;
+      byte x;
+      byte y;
+      byte ps;
 
-   byte c : 1;
-   byte z : 1;
-   byte i : 1;
-   byte d : 1;
-   byte b : 1;
-   byte v : 1;
-   byte n : 1;
-};
+      byte c : 1;
+      byte z : 1;
+      byte i : 1;
+      byte d : 1;
+      byte b : 1;
+      byte v : 1;
+      byte n : 1;
+   };
 
-typedef struct Mem Mem;
-typedef struct CPU CPU;
+   typedef struct Mem Mem;
+   typedef struct CPU CPU;
 
-void reset(CPU* cpu, Mem* mem);
-void initMem(Mem* mem);
-void exec(CPU* cpu, Mem* mem, u32 cycles);
+   void reset(CPU* cpu, Mem* mem);
+   void initMem(Mem* mem);
+   void exec(CPU* cpu, Mem* mem, u32 cycles);
 
+#ifdef __cplusplus
+}
+#endif
 #endif // !CPU_H
