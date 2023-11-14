dee5712cadbb44bfd9e51c38ec90b16ff63b38f5
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2023-11-14 13:08:22 +0100

    Add LDA Indirect Y and add debug code

--- cpu6502_emu/cpu.c
+++ cpu6502_emu/cpu.c
@@ -5,27 +5,34 @@
 #pragma region Private
 
 byte readByteFromPC(word* pc, const RAM* ram, u32* cycles) {
-   byte data = ram->data[*pc];
+   byte val = ram->data[*pc];
+#ifdef _DEBUG
+   printf("DEBUG\t| Read byte [0x%X] from PC: [0x%X]\n", val, *pc);
+#endif //_DEBUG
    (*pc)++;
    (*cycles)--;
 
-   return data;
+   return val;
 }
 
 byte readByteFromAddr(word addr, const RAM* ram, u32* cycles) {
-   byte data = ram->data[addr];
+   byte val = ram->data[addr];
    (*cycles)--;
 
-   return data;
+#ifdef _DEBUG
+   printf("DEBUG\t| Read byte [0x%X] from ADDR: [0x%X]\n", val, addr);
+#endif //_DEBUG
+
+   return val;
 }
 
 word readWordFromPC(word* pc, const RAM* ram, u32* cycles) {
    byte loB = readByteFromPC(pc, ram, cycles);
    byte hiB = readByteFromPC(pc, ram, cycles);
 
-#ifdef DEBUG
-   printf("DEBUG: Read word. loB=[0x%X], hiB=[0x%X]\n", loB, hiB);
-#endif // DEBUG
+#ifdef _DEBUG
+   printf("DEBUG\t| Read word. loB=[0x%X], hiB=[0x%X]\n", loB, hiB);
+#endif // _DEBUG
 
    return (word)(loB | (hiB << 8));
 }
@@ -34,9 +41,9 @@ word readWordFromAddr(word addr, const RAM* ram, u32* cycles) {
    byte loB = readByteFromAddr(addr, ram, cycles);
    byte hiB = readByteFromAddr(addr + 1, ram, cycles);
 
-#ifdef DEBUG
-   printf("DEBUG: Read word from [0x%X+0x%X]. loB=[0x%X], hiB=[0x%X]\n", addr, addr + 1, loB, hiB);
-#endif // DEBUG
+#ifdef _DEBUG
+   printf("DEBUG\t| Read word from [0x%X+0x%X]. loB=[0x%X], hiB=[0x%X]\n", addr, addr + 1, loB, hiB);
+#endif // _DEBUG
 
    return (word)(loB | (hiB << 8));
 }
@@ -49,10 +56,9 @@ void pushWordToStack(RAM* ram, byte* sp, word val, u32* cycles) {
 
    (*cycles) -= 2;
 
-#ifdef DEBUG
-   printf("DEBUG: Pushed [0x%X] to the stack. Current sp: [0x%X]\n", val, *sp);
-#endif // DEBUG
-
+#ifdef _DEBUG
+   printf("DEBUG\t| Pushed [0x%X] to the stack. Current SP: [0x%X]\n", val, *sp);
+#endif // _DEBUG
 }
 
 word popWordFromStack(const RAM* ram, byte* sp, u32* cycles) {
@@ -62,13 +68,21 @@ word popWordFromStack(const RAM* ram, byte* sp, u32* cycles) {
    (*sp)++;
    (*cycles) -= 4;
 
-   word data = (word)((hB << 8) | lB);
-   return data;
+   word val = (word)((hB << 8) | lB);
+
+#ifdef _DEBUG
+   printf("DEBUG\t| Popped [0x%X] from the stack. Current SP: [0x%X]\n", val, *sp);
+#endif //_DEBUG
+
+   return val;
 }
 
-void setNAndZFlags(CPU* cpu) {
+void setLDAFlags(CPU* cpu) {
    cpu->z = (cpu->a == 0);
    cpu->n = (cpu->a & 0b10000000) > 0;
+#ifdef _DEBUG
+   printf("DEBUG\t| Set LDA Flags: Z: [0x%X], N: [0x%X]\n", cpu->z, cpu->n);
+#endif // _DEBUG
 }
 
 #pragma endregion
@@ -88,7 +102,7 @@ RAM* initRAM() {
    }
 
 #ifdef _DEBUG
-   printf("DEBUG: Allocated [%llu B] to RAM\n", MEM_MAX * sizeof(byte));
+   printf("DEBUG\t| Allocated [%llu B] to RAM\n", MEM_MAX * sizeof(byte));
 #endif // _DEBUG
 
 
@@ -100,13 +114,13 @@ void freeRAM(RAM* ram) {
    free(ram->data);
    free(ram);
 #ifdef _DEBUG
-   printf("DEBUG: Freed allocated memory\n");
-#endif
+   printf("DEBUG\t| Freed allocated memory\n");
+#endif //_DEBUG
 }
 
-void resetCPU(CPU* cpu) {
-   cpu->pc = 0xFFFC;
-   cpu->sp = 0x10;
+void resetCPU(CPU* cpu, word sPC) {
+   cpu->pc = sPC;
+   cpu->sp = 0xFF;
 
    cpu->c = 0;
    cpu->z = 0;
@@ -119,14 +133,19 @@ void resetCPU(CPU* cpu) {
    cpu->a = 0;
    cpu->x = 0;
    cpu->y = 0;
+
+#ifdef _DEBUG
+   printf("DEBUG\t| Reset CPU\n");
+#endif // _DEBUG
+
 }
 
 void exec(CPU* cpu, RAM* ram, u32 cycles) {
    while (cycles > 0) {
       byte nextIns = readByteFromPC(&cpu->pc, ram, &cycles);
-#ifdef DEBUG
-      printf("DEBUG: Instruction: 0x%X\n", nextIns);
-#endif
+#ifdef _DEBUG
+      printf("DEBUG\t| Instruction: 0x%X\n", nextIns);
+#endif //_DEBUG
       switch (nextIns) {
       case INS_JSR: {
          word addr = readWordFromPC(&cpu->pc, ram, &cycles);
@@ -143,26 +162,25 @@ void exec(CPU* cpu, RAM* ram, u32 cycles) {
       case INS_LDA_IM: {
          byte val = readByteFromPC(&cpu->pc, ram, &cycles);
          cpu->a = val;
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
       } break;
       case INS_LDA_ZP: {
          byte zpAddr = readByteFromPC(&cpu->pc, ram, &cycles);
          cpu->a = readByteFromAddr(zpAddr, ram, &cycles);
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
       } break;
       case INS_LDA_ZPX: {
          byte zpAddr = readByteFromPC(&cpu->pc, ram, &cycles);
          zpAddr += cpu->x;
-
+         cycles--;
          cpu->a = readByteFromAddr(zpAddr, ram, &cycles);
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
       } break;
       case INS_LDA_ABS: {
          word absAddr = readWordFromPC(&cpu->pc, ram, &cycles);
-         cycles--;
 
          cpu->a = readByteFromAddr(absAddr, ram, &cycles);
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
       } break;
       case INS_LDA_ABSX: {
          word absAddr = readWordFromPC(&cpu->pc, ram, &cycles);
@@ -171,8 +189,9 @@ void exec(CPU* cpu, RAM* ram, u32 cycles) {
          if (crossed) {
             cycles--;
          }
+
          cpu->a = readByteFromAddr(absAddrX, ram, &cycles);
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
       } break;
       case INS_LDA_ABSY: {
          word absAddr = readWordFromPC(&cpu->pc, ram, &cycles);
@@ -181,8 +200,9 @@ void exec(CPU* cpu, RAM* ram, u32 cycles) {
          if (crossed) {
             cycles--;
          }
+
          cpu->a = readByteFromAddr(absAddrY, ram, &cycles);
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
       } break;
       case INS_LDA_INDX: {
          byte addrZp = readByteFromPC(&cpu->pc, ram, &cycles);
@@ -191,10 +211,22 @@ void exec(CPU* cpu, RAM* ram, u32 cycles) {
          word addr = readWordFromAddr(addrZp, ram, &cycles);
          
          cpu->a = readByteFromAddr(addr, ram, &cycles);
-         setNAndZFlags(cpu);
+         setLDAFlags(cpu);
+      } break;
+      case INS_LDA_INDY: {
+         byte addrZp = readByteFromPC(&cpu->pc, ram, &cycles);
+         word addr = readWordFromAddr(addrZp, ram, &cycles);
+         word addrY = addr + cpu->y;
+         const bool crossed = (addr ^ addrY) >> 8;
+         if (crossed) {
+            cycles--;
+         }
+
+         cpu->a = readByteFromAddr(addrY, ram, &cycles);
+         setLDAFlags(cpu);
       } break;
       default: {
-         printf("DEBUG: Unsupported instruction: %d", nextIns);
+         printf("Unsupported instruction: %d\n", nextIns);
       }
       }
    }
--- cpu6502_emu/cpu.h
+++ cpu6502_emu/cpu.h
@@ -1,7 +1,6 @@
 #pragma once
 #ifndef CPU_H
 #define CPU_H
-#define DEBUG
 
 #include <stdlib.h>
 
@@ -9,12 +8,13 @@
 #define INS_JSR 0x20       //Jump to Subroutine. Takes 6 cycles.
 #define INS_RTS 0x60       //Returns to the calling routine. Takes 6 cycles.
 #define INS_LDA_IM 0xA9    //Load accumulator immediate. Takes 2 cycles.
-#define INS_LDA_ZP 0xA5    //Zero page. Takes 3 cycles.
-#define INS_LDA_ZPX 0xB5   //Zero page X. Takes 4 cycles.
-#define INS_LDA_ABS 0xAD   //Absolute. Takes 4 cycles.
-#define INS_LDA_ABSX 0xBD  //Absolute X. Takes 4(+1) cycles.
-#define INS_LDA_ABSY 0xB9  //Absolute Y. Takes 4(+1) cycles
-#define INS_LDA_INDX 0xA1  //Indirect X. Takes 6 cycles.
+#define INS_LDA_ZP 0xA5    //LDA Zero page. Takes 3 cycles.
+#define INS_LDA_ZPX 0xB5   //LDA Zero page X. Takes 4 cycles.
+#define INS_LDA_ABS 0xAD   //LDA Absolute. Takes 4 cycles.
+#define INS_LDA_ABSX 0xBD  //LDA Absolute X. Takes 4(+1) cycles.
+#define INS_LDA_ABSY 0xB9  //LDA Absolute Y. Takes 4(+1) cycles
+#define INS_LDA_INDX 0xA1  //LDA Indirect X. Takes 6 cycles.
+#define INS_LDA_INDY 0xB1  //LDA Indirect Y. Takes 4 cycles.
 
 typedef unsigned char byte;
 typedef unsigned short word;
@@ -45,7 +45,7 @@ struct CPU {
 typedef struct RAM RAM;
 typedef struct CPU CPU;
 
-void resetCPU(CPU* cpu);
+void resetCPU(CPU* cpu, word sPC);
 RAM* initRAM();
 void freeRAM(RAM* ram);
 void exec(CPU* cpu, RAM* ram, u32 cycles);
